b8935056b16dcadbc22b562494c5d5327cdb8eaa
Julien <hengjchan@gmail.com>
2023-12-07 23:26:32 +0800

    successfully modified models to name

--- backend/settings.py
+++ backend/settings.py
@@ -83,23 +83,23 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'cfDggD62F4Gge-d23daFfFfEBAG3e544',
-        'HOST': 'viaduct.proxy.rlwy.net',
-        'PORT': '16190',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'railway',
+#         'USER': 'postgres',
+#         'PASSWORD': 'cfDggD62F4Gge-d23daFfFfEBAG3e544',
+#         'HOST': 'viaduct.proxy.rlwy.net',
+#         'PORT': '16190',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
